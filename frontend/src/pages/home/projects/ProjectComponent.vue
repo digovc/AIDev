263fb631a59cd0a79d7ad90effd3d8a7fa8c7bf6
digovc <rodrigo.conspena@gmail.com>
2025-06-30 18:40:56 -0300

    Permite selecionar o projeto com o enter

--- frontend/src/pages/home/projects/ProjectComponent.vue
+++ frontend/src/pages/home/projects/ProjectComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-hidden bg-gray-900 rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-300 cursor-pointer hover:scale-102" @click="navigateToProject">
+  <div class="overflow-hidden bg-gray-900 rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-300 cursor-pointer hover:scale-102" @click="navigateToProject" tabindex="0" @keydown.enter="navigateToProject">
     <h2 class="text-xl font-semibold mb-2">{{ project.name }}</h2>
     <p v-if="project.description" class="text-sm text-gray-400 mb-2 line-clamp-2">{{ project.description }}</p>
     <div class="text-sm text-gray-300 text-xs overflow-hidden overflow-ellipsis">
