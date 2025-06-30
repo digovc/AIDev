858f59d8a2aa00bbc74ebe2c0182d30021646d57
digovc <rodrigo.conspena@gmail.com>
2025-06-30 18:46:27 -0300

    Ajuste

--- backend/src/services/task-runner.service.js
+++ backend/src/services/task-runner.service.js
@@ -59,6 +59,11 @@ class TaskRunnerService {
 
     const cancelationTokens = this.cancelationTokens.filter(t => t.taskId === taskId);
     cancelationTokens.forEach(t => t.cancel());
+
+    const task = await tasksStore.getById(taskId);
+    task.status = 'backlog';
+    await tasksStore.update(task.id, task);
+
     socketIOService.io.emit('task-not-executing', taskId);
   }
 
